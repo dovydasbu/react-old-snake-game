91953204ab5c2a141c1c203cd1695b21fb9de370
Dovydas Bukauskas <dovydas@bukauskas.net>
2020-05-02 19:12:10 +0300

    Preparing component for npmjs

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('game'));
+export default App;
